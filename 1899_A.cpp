52e3c862cc4ff4048a42992207bafe872cfdc6aa
sagar510 <sagarpal786t@gmail.com>
2025-10-16 17:12:29 +0000

    1899_A

---
+++ 1899_A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        if(n%3){
+            cout<<"First\n";
+        }else{
+            cout<<"Second\n";
+        }
+    }
+
+    return 0;
+}
